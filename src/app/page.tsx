a50d508f865cb9ccc4e9c303cca598a0c94e2bd1
Ademir <ademirperalta@gmail.com>
2025-07-13 22:59:39 -0400

    fix page (#9)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-7xl font-bold text-center">Ademir Peralta</h1>
-      <h2 className="text-5xl font-bold text-center">Staging</h2>
     </main>
   );
 }
