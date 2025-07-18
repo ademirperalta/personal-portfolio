883a2414f687cbc3f02b265d97be7d19d3603096
Ademir Peralta <ademirperalta@gmail.com>
2025-07-18 03:06:26 -0400

    testing things out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-7xl font-bold text-center">Ademir Peralta</h1>
+      <Link href="/gif" className="group">
+        <h1 className="text-7xl font-bold text-center">Ademir Peralta</h1>
+      </Link>
     </main>
   );
 }
